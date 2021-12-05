0911b6eb70e3865fa31d774b860bff93a770099b
Ocawesome101 <ocawesome101@gmail.com>
2021-12-05 12:06:32 -0500

    update: part 2 was slightly more challenging than i expected, i think i've now actually solved it

--- 5/2.lua
+++ 5/2.lua
@@ -12,9 +12,6 @@ local overlaps = 0
 for line in io.lines("5/i") do
   local ax, ay, bx, by = line:match("(%d+),(%d+) %-> (%d+),(%d+)")
   ax, ay, bx, by = tonumber(ax), tonumber(ay), tonumber(bx), tonumber(by)
-  if ax > bx then
-    ax, bx = bx, ax
-  end
   if ax == bx then
     if ay > by then
       ay, by = by, ay
@@ -24,6 +21,9 @@ for line in io.lines("5/i") do
       grid[ax][i] = grid[ax][i] + 1
     end
   elseif ay == by then
+    if ax > bx then
+      ax, bx = bx, ax
+    end
     for i=ax, bx, 1 do
       if grid[i][ay] == 1 then overlaps = overlaps + 1 end
       grid[i][ay] = grid[i][ay] + 1
@@ -32,9 +32,10 @@ for line in io.lines("5/i") do
     local diff = bx - ax
     local diff2 = by - ay
     assert(math.abs(diff) == math.abs(diff2))
-    local step = (diff2 > 0 and 1 or -1)
-    for i=1, diff, 1 do
-      local ix, iy = ax + i - 1, ay + (i - 1) * step
+    local step = math.abs(diff2) == diff2 and 1 or -1
+    local stepX = math.abs(diff) == diff and 1 or -1
+    for i=1, math.abs(diff), 1 do
+      local ix, iy = ax + ((i - 1) * stepX), ay + ((i - 1) * step)
       if grid[ix][iy] == 1 then overlaps = overlaps + 1 end
       grid[ix][iy] = grid[ix][iy] + 1
     end
