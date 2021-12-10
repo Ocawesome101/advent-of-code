ada706bb110b25720d43401beaff8e0d0710dda4
Ocawesome101 <ocawesome101@gmail.com>
2021-12-10 01:18:31 -0500

    finished day 10 like an hour ago

---
+++ 10/1.lua
@@ -0,0 +1,41 @@
+local chars = "[%(%[{<]"
+local closing = "[%)%]}>]"
+local complement = {
+  ["("] = ")",
+  [")"] = "(",
+  ["["] = "]",
+  ["]"] = "[",
+  ['{'] = '}',
+  ['}'] = '{',
+  ['<'] = ">",
+  ['>'] = "<"
+}
+
+local function checkline(l)
+  local stack = {}
+  for c in l:gmatch(".") do
+    if c:match(chars) then
+      stack[#stack+1] = c
+    elseif stack[#stack] == complement[c] then
+      stack[#stack] = nil
+    else
+      return c
+    end
+  end
+end
+
+local score = 0
+for line in io.lines("i") do
+  local bad = checkline(line)
+  if bad == ")" then
+    score = score + 3
+  elseif bad == "]" then
+    score = score + 57
+  elseif bad == "}" then
+    score = score + 1197
+  elseif bad == ">" then
+    score = score + 25137
+  end
+end
+
+print(score)
---
+++ 10/2.lua
@@ -0,0 +1,48 @@
+local chars = "[%(%[{<]"
+local closing = "[%)%]}>]"
+local complement = {
+  ["("] = ")",
+  [")"] = "(",
+  ["["] = "]",
+  ["]"] = "[",
+  ['{'] = '}',
+  ['}'] = '{',
+  ['<'] = ">",
+  ['>'] = "<"
+}
+
+local function checkline(l)
+  local stack = {}
+  for c in l:gmatch(".") do
+    if c:match(chars) then
+      stack[#stack+1] = c
+    elseif stack[#stack] == complement[c] then
+      stack[#stack] = nil
+    else
+      return c
+    end
+  end
+  return nil, table.concat(stack):reverse()
+    :gsub(".", function(k) return complement[k] end)
+end
+
+local scores = {}
+for line in io.lines("i") do
+  local bad, fin = checkline(line)
+  local score = 0
+  if not bad then -- not bad just incomplete
+    fin = fin:gsub(".", function(k)
+      score = score * 5
+      if k == ")" then score = score + 1 end
+      if k == "]" then score = score + 2 end
+      if k == "}" then score = score + 3 end
+      if k == ">" then score = score + 4 end
+      return k
+    end)
+    scores[#scores+1] = score
+  end
+end
+
+table.sort(scores)
+local score = scores[math.ceil(#scores/2)]
+print(score)
